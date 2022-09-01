7b75877303b29ff940db4fd838271943193fe514
Pavel Griva <xturnerrx@gmail.com>
2022-09-01 21:55:01 +0300

    add response dto, Page

--- src/main/java/com/fedag/CSR/dto/response/UserResponse.java
+++ src/main/java/com/fedag/CSR/dto/response/UserResponse.java
@@ -4,15 +4,18 @@ import com.fedag.CSR.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class UserResponse {
 
+    private int id;
     private String email;
     private String firstName;
     private String lastName;
-    private String password;
     private Role role;
+    private LocalDateTime created;
     private String steamLink;
     private int balanceId;
 }
