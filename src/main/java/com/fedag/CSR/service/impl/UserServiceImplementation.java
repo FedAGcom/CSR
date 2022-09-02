5983083a097e64962de02aa1d983edbc9bee3bbe
Vladislav Gorbunov <wint-mil@mail.ru>
2022-09-02 22:05:59 +0300

    Add logger for UserService

--- src/main/java/com/fedag/CSR/service/impl/UserServiceImplementation.java
+++ src/main/java/com/fedag/CSR/service/impl/UserServiceImplementation.java
@@ -8,13 +8,14 @@ import com.fedag.CSR.model.User;
 import com.fedag.CSR.repository.UserRepository;
 import com.fedag.CSR.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImplementation implements UserService {
@@ -23,31 +24,43 @@ public class UserServiceImplementation implements UserService {
     private final UserRepository userRepository;
 
     public Page<UserResponse> getAllUsers(Pageable pageable) {
-        return userMapper.modelToDto(userRepository.findAll(pageable));
+        log.info("Получение всех пользователей");
+        Page<UserResponse> userResponses = userMapper.modelToDto(userRepository.findAll(pageable));
+        log.info("Все пользователи получены");
+        return userResponses;
     }
 
     @Override
     public UserResponse getUser(int id) {
+        log.info("Получение пользователя c id {}", id);
         UserResponse userResponse = null;
         Optional<User> optional = userRepository.findById(id);
         if (optional.isPresent()) {
             User user = optional.get();
             userResponse = userMapper.modelToDto(user);
         }
+        log.info("Получен пользователь с id {}", id);
         return userResponse;
+
     }
 
     @Override
     public void save(UserRequest user) {
+        log.info("Создание пользователя");
         userRepository.save(userMapper.dtoToModel(user));
+        log.info("Пользователь создан");
     }
 
     @Override
     public void deteleUser(int id) {
+        log.info("Удаление пользователя c id {}", id);
         userRepository.deleteById(id);
+        log.info("Пользователь c id {} удален", id);
     }
 
     public void update(UserUpdate user) {
+        log.info("Обновление пользователя с id {}", user.getId());
         userRepository.save(userMapper.dtoToModel(user));
+        log.info("Пользователь с id {} обновлен", user.getId());
     }
 }
