119e4f50dd5cb6bb3ae7543e2f5a44b445fc6b9e
Vladislav Gorbunov <wint-mil@mail.ru>
2022-09-02 21:44:15 +0300

    Steam api first commit

---
+++ src/main/java/com/fedag/CSR/steam/SteamTestClass.java
@@ -0,0 +1,12 @@
+package com.fedag.CSR.steam;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class SteamTestClass {
+    private String classid = "";
+    private String instanceid = "";
+    private String market_name = "";
+}
