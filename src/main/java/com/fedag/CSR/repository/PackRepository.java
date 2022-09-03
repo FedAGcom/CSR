507c1f2ade3f8cc39d1a747f992ebaab39fc624d
gamakarenko <gleb.makarenko.2022@yandex.ru>
2022-09-03 22:52:58 +0300

    Add repository annotation

--- src/main/java/com/fedag/CSR/repository/PackRepository.java
+++ src/main/java/com/fedag/CSR/repository/PackRepository.java
@@ -2,8 +2,10 @@ package com.fedag.CSR.repository;
 
 import com.fedag.CSR.model.Pack;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 
+@Repository
 public interface PackRepository extends JpaRepository<Pack, BigDecimal> {
 }
