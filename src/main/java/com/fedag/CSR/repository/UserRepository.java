4160c1377a74d9dd298b773ff4998342e15a4fff
Pavel Griva <xturnerrx@gmail.com>
2022-09-03 10:01:56 +0300

    change id's type

--- src/main/java/com/fedag/CSR/repository/UserRepository.java
+++ src/main/java/com/fedag/CSR/repository/UserRepository.java
@@ -5,6 +5,8 @@ import com.fedag.CSR.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, BigDecimal> {
 }
