e1ce759a4ed115f93790fdcc189c1eb50142b97e
MaxiRage <iarostiuk@ya.ru>
2022-10-10 16:53:16 +0300

    hide port 8080 in url (replace with 80)

--- src/main/java/com/fedag/CSR/service/impl/UserAuthImpl.java
+++ src/main/java/com/fedag/CSR/service/impl/UserAuthImpl.java
@@ -67,7 +67,8 @@ public class UserAuthImpl implements UserAuth {
         user.setConfirmationToken(token);
         userRepository.save(user);
 
-        String link = "http://5.101.51.35:8080/api/v1/activate/confirm?token=" + token;
+        String link = "http://5.101.51.35:80/api/v1/activate/confirm?token=" + token;
+//        String link = "http://localhost:80/api/v1/activate/confirm?token=" + token;
         emailService.send(registrationRequest.getEmail(), registrationRequest.getFirstName(), link);
 
         responseMap.put("error", false);
