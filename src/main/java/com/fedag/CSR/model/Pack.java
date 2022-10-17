4fc14ec744ec10bc095fb3d3a774b684c60a04b4
MaxiRage <iarostiuk@ya.ru>
2022-10-17 20:28:06 +0300

    Model Pack @Getter/@Setter

--- src/main/java/com/fedag/CSR/model/Pack.java
+++ src/main/java/com/fedag/CSR/model/Pack.java
@@ -1,16 +1,15 @@
 package com.fedag.CSR.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
