2b362b3f1f93d6e726b7e4af8d05ae4cd57cd8ad
MaxiRage <iarostiuk@ya.ru>
2022-12-07 18:55:19 +0300

    Deleting images after restarting containers and fix decimal for win chance

--- src/main/resources/liquibase/db_changelog/create_table_pack_item.sql
+++ src/main/resources/liquibase/db_changelog/create_table_pack_item.sql
@@ -3,7 +3,7 @@ CREATE TABLE pack_item
     id SERIAL PRIMARY KEY ,
     pack_id BIGINT NOT NULL,
     item_id BIGINT NOT NULL,
-    win_chance DECIMAL(10,2) NOT NULL,
+    win_chance DECIMAL(6,4) NOT NULL,
     FOREIGN KEY (pack_id) REFERENCES Pack(id),
     FOREIGN KEY (item_id) REFERENCES Item(id)
 )
